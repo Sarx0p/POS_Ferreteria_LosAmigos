914daa56a8f56e393fede7563ddc694b613d77f0
Jommintt <lopezalejandra2027@gmail.com>
2025-11-25 09:54:37 -0600

    commit- como ute quiera

--- Proyecto_POSFerreteria/Presentacion/FrmLogin.cs
+++ Proyecto_POSFerreteria/Presentacion/FrmLogin.cs
@@ -21,5 +21,15 @@ namespace Proyecto_POSFerreteria
         {
 
         }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
