7fe94d4bc509aaad048b04efefb6d7af910eb46a
zarck <zarck430@gmail.com>
2025-11-27 18:49:51 -0600

    Login finalizado

--- Proyecto_POSFerreteria/Datos/Conexion.cs
+++ Proyecto_POSFerreteria/Datos/Conexion.cs
@@ -8,9 +8,9 @@ namespace Proyecto_POSFerreteria.Datos
 {
     public class Conexion
     {
-        public static string Cadena = @"Data Source=DESKTOP-503K00D\SQLEXPRESS;Initial Catalog=POSFerreteriaDB;Integrated Security=True";
+        public static string Cadena = @"Data Source=DESKTOP-K12R4B5\SQLEXPRESS;Initial Catalog=FerreteriaElAmigo;Integrated Security=True";
     }
 }
 // Computadora de Nomo de jardin DESKTOP-503K00D\SQLEXPRESS
 //Computadora de Moises de escritorio DESKTOP-503K00D\SQLEXPRESS
-//Laptop de  MOises 
+//Laptop de  MOises DESKTOP-K12R4B5\SQLEXPRESS
