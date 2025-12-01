32d007c7bded5c19ca46e0adde4d3b59f995a03d
Zarck <zarck430@gmail.com>
2025-12-01 02:05:42 -0600

    SOLUCION

---
+++ Proyecto_POSFerreteria/utilidades/CriptoDPAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_POSFerreteria.Utilidades
+{
+    public static class CryptoDPAPI 
+    { public static byte[] CifrarContrasena(string contrasena)
+        { 
+            if (contrasena == null) throw new ArgumentNullException(nameof(contrasena)); 
+            var datos = Encoding.UTF8.GetBytes(contrasena); 
+            return ProtectedData.Protect(datos, null, DataProtectionScope.LocalMachine);
+        }
+        public static string DesencriptarContrasena(byte[] blob) 
+        { 
+            if (blob == null) return null;
+            var dec = ProtectedData.Unprotect(blob, null, DataProtectionScope.LocalMachine);
+            return Encoding.UTF8.GetString(dec); } }
+}
