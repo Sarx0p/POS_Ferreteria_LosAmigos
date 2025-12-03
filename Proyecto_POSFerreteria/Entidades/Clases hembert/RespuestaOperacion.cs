24d7587fc1d954d31f1c68e9e89a504812123bde
Hembert Arteaga <ap25002@itcha.edu.sv>
2025-12-03 08:54:19 -0600

    Clases creadas para el CRUD venta

---
+++ Proyecto_POSFerreteria/Entidades/Clases hembert/RespuestaOperacion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_POSFerreteria.Entidades.Clases_hembert
+{
+    internal class RespuestaOperacion
+    {
+    }
+}
